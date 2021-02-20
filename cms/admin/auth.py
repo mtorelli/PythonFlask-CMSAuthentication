ff07cee735c5a1921732f2e4015eded78e9cdc1f
Mike Torelli <mtorelli@gmail.com>
2021-02-20 10:35:16 -0500

    Add protected decorator

--- cms/admin/auth.py
+++ cms/admin/auth.py
@@ -3,3 +3,8 @@ from cms.admin.models import User
 from flask import render_template, request, redirect, url_for, flash
 from flask import session, g
 from functools import wraps
+
+
+def protected(route_function):
+    def wrapped_route_function(**kwargs):
+        return route_function(**kwargs)
