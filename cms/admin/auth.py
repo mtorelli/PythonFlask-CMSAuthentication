42baf0f20cad373da7e09d856a07645badd6b9bc
Mike Torelli <mtorelli@gmail.com>
2021-02-20 10:40:24 -0500

    Add redirect if user is not logged in

--- cms/admin/auth.py
+++ cms/admin/auth.py
@@ -6,5 +6,9 @@ from functools import wraps
 
 
 def protected(route_function):
+    @wraps(route_function)
     def wrapped_route_function(**kwargs):
+        if g.user is None:
+            return redirect(url_for('admin.login'))
         return route_function(**kwargs)
+    return wrapped_route_function
